f922e51c78c4799e3408cd81133e9fbbc3dbe08a
Jose Pablo <josepablo.monge@gmail.com>
2019-10-19 18:55:08 -0600

    Backing up website
    Website code in progress

--- ProjectManagement/wwwroot/js/site.js
+++ ProjectManagement/wwwroot/js/site.js
@@ -5,9 +5,55 @@
 function AppViewModel() {
     var self = this;
 
-    self.project = ko.observableArray([
-        { name: 'projectid' },
-        { name: 'credits' },
-        { name: 'status' }
-    ]);
+    self.SelectedUserId = ko.observable();
+    self.UserList = ko.observableArray();
+    self.ProjectList = ko.observableArray();
+
+    self.SelectedUserId.subscribe(function (selectedUser) {
+        $.ajax({
+            url: '/api/project/?userid=' + selectedUser.id(),
+            type: "GET",
+            datatype: 'json',
+            success: function (data) {
+                var mappedProject = $.map(data, function (project) { return new ProjectViewModel(project); });
+                self.ProjectList(mappedProject);
+            },
+            error: function (x) {
+                alert(x.statusText);
+            }
+        });
+    });
+
+    self.GetUsers = function () {
+        $.ajax({
+            type: "GET",
+            dataType: "json",
+            url: '/api/user',
+            success: function (data) {
+                var mappedUser = $.map(data, function (user) { return new UserViewModel(user); });
+                self.UserList(mappedUser);
+            },
+            error: function () {
+
+            }
+        });
+    };
+
+    self.GetUsers();
+}
+
+function UserViewModel(data) {
+    var self = this;
+    self.id = ko.observable(data.id);
+    self.userName = ko.observable(data.firstName + " " + data.lastName);
+}
+
+function ProjectViewModel(data) {
+    var self = this;
+    self.projectId = ko.observable(data.projectId);
+    self.credits = ko.observable(data.credits);
+    self.status = ko.observable(data.status);
+    self.startDate = ko.observable(data.startDate);
+    self.endDate = ko.observable(data.endDate);
+    self.timeToStart = ko.observable(data.timeToStart);
 }
